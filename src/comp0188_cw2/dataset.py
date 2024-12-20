f198b8aefbffe485425686328dd14c69205bead6
Iyassou Shimels <s.iyassou@gmail.com>
2024-12-20 16:54:31 +0000

    Apply transforms at initialisation

--- src/comp0188_cw2/dataset.py
+++ src/comp0188_cw2/dataset.py
@@ -11,13 +11,17 @@ from torchvision.transforms import v2
 
 class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, directory: Path, transforms: dict[str, v2.Transform], dev: torch.device):
-        self.transforms = transforms
         self.device = dev
+        self.observations = []
         h5_files = sorted(
             directory.glob("*.h5"),
             key=lambda x: int(x.stem.split("_")[-1]), # ascending order of suffix number
         )
-        self.observations = [obs for file in h5_files for obs in Chunk(file)[:]]
+        for file in h5_files:
+            for obs in Chunk(file)[:]:
+                for key, transform in transforms.items():
+                    setattr(obs, key, transform(getattr(obs, key)))
+                self.observations.append(obs)
 
     def __len__(self) -> int:
         return len(self.observations)
@@ -26,10 +30,6 @@ class CustomDataset(torch.utils.data.Dataset):
         if not (0 <= j < len(self)):
             raise IndexError(f"Index {j} out-of-bounds for container of length {len(self)}")
         obs: Observation = self.observations[j]
-
-        if self.transforms:
-            for key, transform in self.transforms.items():
-                setattr(obs, key, transform(getattr(obs, key)))
 
         # Observations
         images = torch.concat((obs.front_cam_ob, obs.mount_cam_ob), dim=0)
