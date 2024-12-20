48d708297cbc874e896ada801054359c30cda5bc
Iyassou Shimels <s.iyassou@gmail.com>
2024-12-20 12:59:16 +0000

    Shift IO responsibility
    `Chunk` is now responsible for loading files from disk, `Observation` is now but a receptable

--- src/comp0188_cw2/datatypes.py
+++ src/comp0188_cw2/datatypes.py
@@ -20,23 +20,18 @@ class GripperAction(enum.Enum):
     CLOSE    = 2
 
 # No "prompts" or "rewards" in the preprocessed versions we're working with.
-_ObservationKeys = typing.Literal[
+# "terminals" isn't being used right now.
+_ObservationBaseAttribute = typing.Literal[
     "actions", "ee_cartesian_pos_ob", "ee_cartesian_vel_ob", "front_cam_ob",
-    "joint_pos_ob", "mount_cam_ob", "terminals"
+    "joint_pos_ob", "mount_cam_ob"
 ]
-_ObservationTypes = V4, V7, V6, Grayscale, V2, Grayscale, np.bool_
+_ObservationBaseTypes = V4, V7, V6, Grayscale, V2, Grayscale, np.bool_
 _ObservationBase = dataclasses.make_dataclass(
-    "_ObservationBase", zip(typing.get_args(_ObservationKeys), _ObservationTypes)
+    "_ObservationBase", zip(typing.get_args(_ObservationBaseAttribute), _ObservationBaseTypes)
 )
 
 @dataclasses.dataclass
 class Observation(_ObservationBase):
-    @classmethod
-    def from_h5(cls, f: h5py.File, j: int) -> "Observation":
-        data = {k: f[k][j] for k in typing.get_args(_ObservationKeys)}
-        data["terminals"] = bool(data["terminals"])
-        return Observation(**data)
-
     @property
     def gripper_action(self) -> GripperAction:
         return GripperAction(self.actions[3])
@@ -56,27 +51,28 @@ class Chunk:
     def __getitem__(self, j: int|slice) -> Observation | list[Observation]:
         if not isinstance(j, (int, slice)):
             raise TypeError(f"Unsupported index type: {repr(type(j))}")
+        
         if isinstance(j, int):
             if j < 0:
                 j %= len(self)
             if not (0 <= j < len(self)):
                 raise IndexError(f"Index {j} out of bounds for container of length {len(self)}")
-            with h5py.File(self.file, "r") as f:
-                return Observation.from_h5(f, j)
-        start, stop, step = j.start, j.stop, j.step
-        if start is not None and start < 0:
-            start %= len(self)
-        if stop is not None and stop < 0:
-            stop %= len(self)
-        start = start or 0
-        stop = stop or len(self)
-        step = step or 1
-        if start > stop:
-            raise ValueError(f"Invalid slice {j}")
+        else:
+            start, stop, step = j.start, j.stop, j.step
+            if start is not None and start < 0:
+                start %= len(self)
+            if stop is not None and stop < 0:
+                stop %= len(self)
+            start = start or 0
+            stop = stop or len(self)
+            step = step or 1
+            if start > stop:
+                raise ValueError(f"Invalid slice {j}")
+            j = range(start, stop, step)
+
         with h5py.File(self.file, "r") as f:
-            return list(
-                map(
-                    lambda x: Observation.from_h5(f, x),
-                    range(start, stop, step)
-                )
-            )
+            data = {k: f[k][j] for k in typing.get_args(_ObservationBaseAttribute)}
+
+        if isinstance(j, int):
+            return Observation(**data)
+        return [Observation(**{k: v[m] for k, v in data.items()}) for m in j]
