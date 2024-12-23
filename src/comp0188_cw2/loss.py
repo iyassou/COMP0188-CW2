fc8ad7547bd56365a222b0d4dcb68f234acf46c6
Iyassou Shimels <s.iyassou@gmail.com>
2024-12-23 22:52:55 +0000

    Implement beta-VAE loss

--- src/comp0188_cw2/loss.py
+++ src/comp0188_cw2/loss.py
@@ -1,4 +1,7 @@
 import torch
+import typing
+
+BetaVAEReconstructionLoss = typing.Literal["mse", "bce"]
 
 class BalancedMSECrossEntropyLoss(torch.nn.modules.loss._Loss):
     """Balanced loss, equally weighting the MSE loss of the position-velocity
@@ -28,3 +31,57 @@ class BalancedMSECrossEntropyLoss(torch.nn.modules.loss._Loss):
         pv_loss = self.pos_vel_loss(prediction[:, :3], actual[:, :3])
         g_loss = self.gripper_loss(prediction[:, 3:], actual[:, 3:])
         return torch.mean(pv_loss + g_loss)
+
+class BetaVAELoss(torch.nn.modules.loss._Loss):
+    """Parametrised loss function for beta variational autoencoders.
+    Returns the sum of the reconstruction loss and the beta-weighted KL divergence."""
+    def __init__(self, beta: float, reconstruction_loss: BetaVAEReconstructionLoss):
+        super().__init__()
+        if reconstruction_loss not in typing.get_args(BetaVAEReconstructionLoss):
+            raise ValueError(
+                f"unrecognised `reconstruction_loss` {repr(reconstruction_loss)}, "
+                f"must be one of: {', '.join(map(repr, typing.get_args(BetaVAEReconstructionLoss)))}"
+            )
+        
+        self.beta = beta
+        if reconstruction_loss == "mse":
+            self.reconstruction_loss = torch.nn.MSELoss(reduction="mean")
+        else:
+            self.reconstruction_loss = torch.nn.BCELoss(reduction="mean")
+
+    def forward(
+            self, 
+            prediction: tuple[torch.Tensor, torch.Tensor, torch.Tensor],
+            actual: tuple[torch.Tensor, torch.Tensor]
+        ) -> torch.Tensor:
+        """Accepts a tuple comprising the reconstruction and two latent space distribution
+        parameters (mean and log of the variance), and the input data fed to the model.
+        
+        Parameters
+        ----------
+        prediction: tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+            (0) VAE's reconstruction, with shape            [batch_size, 2, 224, 224]
+            (1) Latent space's mean, with shape             [batch_size, latent_space_dimensions]
+            (2) Log of latent space's variance, with shape  [batch_size, latent_space_dimensions]
+        actual: tuple[torch.Tensor, torch.Tensor]
+            (0) `images`, with shape                        [batch_size, 2, 224, 224]
+            (1) `dynamics`, with shape                      [batch_size, 15]
+
+        Notes
+        -----
+        DOES NOT USE THE DYNAMICS DATA! It's just convenient to keep the `forward` function signatures
+        the same and drop the unused data on a per-case basis rather than change everything elsewhere.
+        
+        Uses the closed-form of the KL divergence between an arbitrary Gaussian and a
+        normal Gaussian rather than `torch.nn.KLDivLoss`.
+
+        Returns
+        -------
+        torch.Tensor
+            Sum of the reconstruction loss and the beta-weighted KL divergence"""
+        reconstruction, mu, logvar = prediction
+        batch_size = reconstruction.size(0)
+        images, _ = actual
+        r_loss = self.reconstruction_loss(reconstruction, images)
+        kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) / batch_size
+        return r_loss + self.beta * kl_loss
